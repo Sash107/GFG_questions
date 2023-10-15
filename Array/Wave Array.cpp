b0bf929cd806d2d061fb5f269568d020b87799f1
Suyash Upadhyay <66892274+Sash107@users.noreply.github.com>
2023-10-16 00:14:29 +0530

    Create Wave Array.cpp

---
+++ Array/Wave Array.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+// #include <algorithm>
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to sort the array into a wave-like array.
+    void convertToWave(int n, vector<int>& arr){
+        int i;
+        for(i=0;i<n;i=i+2){
+            if(n%2==0){
+                swap(arr[i],arr[i+1]);
+            }
+            if(n%2!=0){
+                if(i==n-1) break;
+                swap(arr[i],arr[i+1]);
+            }
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    cin>>t; //Input testcases
+    while(t--) //While testcases exist
+    {
+        cin>>n; //input size of array
+        vector<int> a(n); //declare vector of size n
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //input elements of array
+        sort(a.begin(),a.end());
+        Solution ob;
+        ob.convertToWave(n, a);
+
+        for(int i=0;i<n;i++)
+            cout<<a[i]<<" "; //print array
+            
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
